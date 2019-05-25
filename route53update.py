db8ad2a797b6fabdda963a34314760ce8097d477
Jesse <sora4222@gmail.com>
2019-05-25 16:27:21 +1000

    Adds more consistent logging

--- route53update.py
+++ route53update.py
@@ -38,12 +38,12 @@ def get_external_ip() -> str:
             ip_backup = urlopen('http://ip.42.pl/raw').read()
             logging.error(message_formatter(
                 ("Level", "Error"),
-                ("message", "The ip address obtained was not valid"),
-                ("opendns ip address", ip)))
+                ("Message", "The ip address obtained was not valid"),
+                ("Opendns ip address", ip)))
             if not is_ip_address(ip_backup):
                 logging.error(message_formatter(("Level", "Error"),
-                                                ("message", "The backup ip-address was not valid"),
-                                                ("ip.42.pl ip address", ip_backup)
+                                                ("Message", "The backup ip-address was not valid"),
+                                                ("Ip.42.pl ip address", ip_backup)
                                                 ))
             return ip_backup
         return ip
@@ -89,15 +89,15 @@ def is_ip_same_as_previous(new_ip: str, ip_log_location: str) -> bool:
     try:
         with open(ip_log_location, mode="r") as ip_file:
             previous_ip = ip_file.read()
-            logging.info(message_formatter(("previous ip", previous_ip),
+            logging.info(message_formatter(("Previous ip", previous_ip),
                                            ("Level", "Info")
                                            ))
             return previous_ip == new_ip
     # If the file hasn't been found it will be made later
     except FileNotFoundError as exc:
         logging.warning(message_formatter(
-            ("Message", "Ip address log not found"),
             ("Level", "Warning"),
+            ("Message", "Ip address log not found"),
             ("Error", str(exc)),
             ("Ignorable", "True")
         ))
@@ -139,19 +139,19 @@ if __name__ == '__main__':
     logging.basicConfig(filename=config.logging_location,
                         format='%(message)s',
                         level=logging.INFO)
-    logging.info(message_formatter(("action", "starting process")))
+    logging.info(message_formatter(("Action", "Starting process")))
 
     record: str
     ip: str = get_external_ip()
     # Only run this if the ip-address has changed
     if not is_ip_same_as_previous(ip, config.ip_log_location):
         for record in config.records:
-            logging.info(message_formatter(("Level", "info"),
+            logging.info(message_formatter(("Level", "Info"),
                                            ("Action", f"starting update record: {record}")))
             update_record(record, ip, config.hosted_zone_id)
-            logging.info(message_formatter(("Level", "info"),
+            logging.info(message_formatter(("Level", "Info"),
                                            ("Action", f"finished update record: {record}")))
         write_ip_address_to_file(config.ip_log_location, ip)
     else:
-        logging.info(message_formatter(("Level", "info"),
+        logging.info(message_formatter(("Level", "Info"),
                                        ("Message", "The ip-address has not changed.")))
